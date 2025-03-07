ca3cf49dc27e7582a97dff164b516ae4ed43d402
bishal9874 <bm8130377@gmail.com>
2025-03-07 12:00:41 +0530

    addition

--- Hello.c
+++ Hello.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
 int main(){
     printf("Hello world");
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
 }
